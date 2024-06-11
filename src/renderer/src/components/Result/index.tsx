4b3683a97bef77e1dee5679fe138fc844de7090d
sniggiw <sniggiw@qq.com>
2024-06-11 22:57:00 +0800

    feat:complete the select style and finish the func of clipboard

--- src/renderer/src/components/Result/index.tsx
+++ src/renderer/src/components/Result/index.tsx
@@ -5,14 +5,20 @@ export default function Result() {
   const { data } = useCode()
   const [currentIndex, setCurrentIndex] = useState(0)
   const handleKeyEvent = (e: KeyboardEvent) => {
-    if (data.length === 0) return setCurrentIndex(0)
+    if (data.length === 0) {
+      setCurrentIndex(0)
+      navigator.clipboard.writeText('')
+      return
+    }
     switch (e.code) {
       case 'ArrowUp':
-        setCurrentIndex((prev) => (prev - 1 <= 0 ? data.length - 1 : prev - 1))
+        setCurrentIndex((prev) => (prev - 1 < 0 ? data.length - 1 : prev - 1))
         break
       case 'ArrowDown':
         setCurrentIndex((prev) => (prev + 1 >= data.length ? 0 : prev + 1))
         break
+      case 'Enter':
+        navigator.clipboard.writeText(data[currentIndex].content)
     }
   }
   useEffect(() => {
@@ -20,12 +26,11 @@ export default function Result() {
     return () => {
       document.removeEventListener('keydown', handleKeyEvent)
     }
-  }, [data])
+  }, [data, currentIndex])
   return (
-    <div className="bg-slate-50 px-3 rounded-bl-lg rounded-br-lg -mt-[8px]">
-      {currentIndex}
-      {data.map((item) => (
-        <div key={item.id} className="text-slate-700  truncate mb-2">
+    <div className="bg-slate-50 px-3 rounded-bl-lg rounded-br-lg -mt-[8px] overflow-hidden">
+      {data.map((item, index) => (
+        <div key={item.id} className={`text-slate-700 truncate mb-2 ${currentIndex == index ? 'bg-orange-400 text-white rounded-md' : ''}`}>
           {item.content}
         </div>
       ))}
