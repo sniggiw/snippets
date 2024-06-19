54b5d10d3c159293cb74baf0859032557cfa1d50
wengweijian <sniggiw@qq.com>
2024-06-19 09:52:49 +0800

    feat:useSearch hook

---
+++ src/renderer/src/hooks/useSearch.ts
@@ -0,0 +1,21 @@
+import { useState, ChangeEvent } from 'react'
+import useCode from './useCode'
+import { codes } from '@renderer/data'
+
+export default () => {
+  const { setData } = useCode()
+  const [search, setSearch] = useState('')
+  const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value)
+    setData(
+      codes.filter((code) =>
+        code.content.toLowerCase().includes(e.target.value.toLowerCase() || '@@@@@')
+      )
+    )
+  }
+
+  return {
+    handleSearch,
+    search
+  }
+}
